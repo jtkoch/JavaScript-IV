f466e1fd93402f63a35ca36643c519ea65e778ea
Jensen Koch <pdpdrums12@Jensens-MacBook-Pro.local>
2019-10-10 17:05:59 -0500

    commit while working on lambda-classes.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,19 @@
 // CODE here for your Lambda Classes
+
+class Person {
+  constructor([properties]) {
+    this.name = properties.name;
+    this.age = properties.age;
+    this.location = properties.location;
+  }
+  speak() {
+    console.log(`Hello my name is ${this.name}, I am from ${this.location}.`);
+  }
+}
+
+class Instructor extends Person {
+  constructor(instructorProperties) {
+    super(instructorProperties);
+    
+  }
+}
